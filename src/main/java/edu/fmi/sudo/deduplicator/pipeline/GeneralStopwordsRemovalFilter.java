5352f287b985bcf6ac73ef1d2da9e71c09f22125
miromir2217 <miromir2217@gmail.com>
2017-01-21 15:40:07 +0200

    General Stopwords filter fixed and tested to work properly

--- src/main/java/edu/fmi/sudo/deduplicator/pipeline/GeneralStopwordsRemovalFilter.java
+++ src/main/java/edu/fmi/sudo/deduplicator/pipeline/GeneralStopwordsRemovalFilter.java
@@ -6,12 +6,12 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class GeneralStopwordsRemovalFilter {
-    private static final String stopwordsFile = "resources/pipeline/stopwords.txt";
+    private static final String stopwordsFile = "src\\main\\resources\\pipeline\\stopwords.txt";
 
     public static QuestionAnswers process(QuestionAnswers qa) {
         OriginalQuestion oq = qa.getQuestion();
@@ -35,22 +35,25 @@ public class GeneralStopwordsRemovalFilter {
             comment.setText(removeStopwords(comment.getText()));
         }
 
-        return null;
+        return qa;
     }
 
     private static String removeStopwords(String text) {
-        Set<String> stopwords = new HashSet<>();
+        // add a dummy word to the end of the text in order to avoid the stream filer operation
+        // returning an empty result in case the text consists entirely of stop words
+        text += " dummy";
 
         try(Stream<String> stream = Files.lines(Paths.get(stopwordsFile))) {
-            stream.forEach(x -> stopwords.add(x));
+            final Set<String> stopwords = stream.collect(Collectors.toSet());
+            String[] words = text.split(" ");
+            return Arrays.stream(words)
+                    .filter(x -> !stopwords.contains(x))
+                    .reduce((x, y) -> x + " " + y)
+                    .get();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        String[] words = text.split(" ");
-        return Arrays.stream(words)
-                .filter(x -> stopwords.contains(x))
-                .reduce((x, y) -> x + " " + y)
-                .get();
+        return null;
     }
 }
